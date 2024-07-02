d3aafee0efe9a15a2777c3eb2f69a89d8cdadf64
damalpol <122043310+damalpol@users.noreply.github.com>
2024-07-02 12:53:24 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, 'dist'), // Corrected from 'output.path' to 'outDir'
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]',
+      },
+    },
+    base: './', // Ensures all paths are relative
+  },
 })
